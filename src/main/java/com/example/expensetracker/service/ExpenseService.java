7c0590ef7bd25f53d406cf566c302c1c87941353
= <=>
2021-07-21 18:40:34 -0300

    primeiro commit

---
+++ src/main/java/com/example/expensetracker/service/ExpenseService.java
@@ -0,0 +1,19 @@
+package com.example.expensetracker.service;
+
+import java.util.List;
+
+import com.example.expensetracker.model.Expense;
+
+public interface ExpenseService {
+
+	
+
+	List<Expense> findAll();
+	
+	Expense save(Expense expense);
+	
+	Expense findById(Long id);
+	
+	void delete(Long id);
+	
+}
